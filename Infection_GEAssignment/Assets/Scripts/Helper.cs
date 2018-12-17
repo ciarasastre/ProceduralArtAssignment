cf2139f2a4ae9b2381db1c05cffabd4e37543d7b
ciarasastre <sastreciara@hotmail.com>
2018-12-17 22:25:17 +0000

    Adding terrain script

---
+++ Infection_GEAssignment/Assets/Scripts/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour {
+
+    [Range(2, 256)] // Maximum range 256 
+    public int resolution = 10;
+
+    MeshFilter[] meshFilters;
+    terrainFace[] terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+    void Initialize()
+    {
+        meshFilters = new MeshFilter[6];
+        terrainFaces = new terrainFace[6];
+
+        Vector3[] directions = {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back}; 
+
+        for(int i=0; i < 6; i++)
+        {
+            GameObject meshObj = new GameObject("mesh");
+            meshObj.transform.parent = transform;
+
+            meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+            meshFilters[i].sharedMesh = new Mesh();
+
+            terrainFaces[i] = new terrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+
+    }
+
+    void GenerateMesh()
+    {
+        foreach (terrainFace face in terrainFaces)
+        {
+
+        }
+    }
+}
