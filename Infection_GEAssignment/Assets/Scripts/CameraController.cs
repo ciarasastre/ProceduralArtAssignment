efd2e5b42349e70d1b3fc5b74228a1f21fa20d85
ciarasastre <sastreciara@hotmail.com>
2018-12-18 21:26:16 +0000

    Added controller

---
+++ Infection_GEAssignment/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Takes cursor away to view game
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //This moves the body
+        float vert = Input.GetAxis("Vertical") * speed;
+        float hor = Input.GetAxis("Horizontal") * speed;
+        float up = Input.GetAxis("Jump") * speed; // Controls up and down
+
+        vert *= Time.deltaTime;
+        hor *= Time.deltaTime;
+        up *= Time.deltaTime;
+
+        transform.Translate(hor, up, vert);
+
+        //Get your cursor back
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Infection_GEAssignment/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+
+    Vector2 look;
+    Vector2 smooth; //Smooths down movement of mouse
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject controller;
+    
+	void Start ()
+    {
+        controller = this.transform.parent.gameObject;
+	}
+	
+	void Update ()
+    {
+        //Controlls mouse movement
+        var mouseM = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseM = Vector2.Scale(mouseM, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        //smoothing vector slows movement
+        smooth.x = Mathf.Lerp(smooth.x, mouseM.x, 1f / smoothing); //Prevents it from jolting from 1 movement to another
+        smooth.y = Mathf.Lerp(smooth.y, mouseM.y, 1f / smoothing);
+        look += smooth; // Apply to controller
+
+
+        transform.localRotation = Quaternion.AngleAxis(-look.y, Vector3.right); //-look.y Inverts it to look up and down
+        //When hes turning whole "body" turn with him
+        controller.transform.localRotation = Quaternion.AngleAxis(look.x, controller.transform.up);
+    }
+}
