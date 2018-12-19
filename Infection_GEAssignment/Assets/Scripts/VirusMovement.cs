f80e6257fb23f86d41f4b025cddfe6902c244328
ciarasastre <sastreciara@hotmail.com>
2018-12-19 21:51:30 +0000

    Virus moves towards cell

--- Infection_GEAssignment/Assets/Scripts/VirusMovement.cs
+++ Infection_GEAssignment/Assets/Scripts/VirusMovement.cs
@@ -9,6 +9,13 @@ public class VirusMovement : MonoBehaviour {
     public float twist = 20;
     public float maxRotationSpeed = 20;
     private float rotationSpeed;
+    
+    public Transform target;
+    public float speed;
+
+    float speed2 = 5f;
+    //adjust this to change how high it goes
+    float height = 0.5f;
 
     // Use this for initialization
     void Start ()
@@ -21,11 +28,33 @@ public class VirusMovement : MonoBehaviour {
         //This prevents it from jolting
         rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
         transform.Rotate(Random.Range(-twist, twist), 0f, 0f);
+        
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        //Allows it to spin
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        //transform.position += Vector3.forward * Time.deltaTime;
+
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void VirusStandBy()
+    {
+        //get the Virus current position
+        Vector3 pos = transform.position;
+
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed2);
+
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
     }
 }
